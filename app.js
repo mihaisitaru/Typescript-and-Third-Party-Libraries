f6c19f48525625fa47032f1189ea4834e4bb1bc7
mihaisitaru <mihai.sitaru@yahoo.com>
2019-08-04 21:59:08 +0300

    Section 10 - done

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // declare var $: any
 // better solution: create *.d.ts file (or npm install them) for declaration purpose,
 // typescript will automatically consider such files
-
 $('button').click(function () {
     console.log('Button clicked!');
-  });
+});
