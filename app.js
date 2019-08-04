5c06da56a52748975f5f8d78aba83bfe2962db3c
mihaisitaru <mihai.sitaru@yahoo.com>
2019-08-04 22:25:03 +0300

    Adding jQuery through systemjs import

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+"use strict";
 // declare var $: any
 // better solution: create *.d.ts file (or npm install them) for declaration purpose,
 // typescript will automatically consider such files
-$('button').click(function () {
-    console.log('Button clicked!');
-});
+Object.defineProperty(exports, "__esModule", { value: true });
+// $('button').click(function () {
+//     console.log('Button clicked!');
+//   });
+require("jQuery");
+$("#app").css({ "width": "10%", "background-color": "gray", "box-shadow": "1px 2px 5px rgba(0,0,0,0.7)" });
--- app.ts
+++ app.ts
@@ -2,6 +2,9 @@
 // better solution: create *.d.ts file (or npm install them) for declaration purpose,
 // typescript will automatically consider such files
 
-$('button').click(function () {
-    console.log('Button clicked!');
-  });
+// $('button').click(function () {
+//     console.log('Button clicked!');
+//   });
+
+import "jQuery";
+$("#app").css({"width": "10%", "background-color": "gray", "box-shadow": "1px 2px 5px rgba(0,0,0,0.7)"});
